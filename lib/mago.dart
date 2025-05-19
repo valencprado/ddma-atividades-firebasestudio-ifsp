8f5d6f939f45dac0cb55c5821ca81ba1059e6753
valenpschool <valenpschool@gmail.com>
2025-05-19 23:54:00 +0000

    lição manipulação JSON

---
+++ lib/mago.dart
@@ -0,0 +1,62 @@
+class Mago {
+  String? nome;
+  int? idade;
+  int? nivelMagico;
+  double? inteligencia;
+  double? mana;
+  int? afinidadeFogo;
+  int? afinidadeAgua;
+  int? afinidadeTerra;
+  int? afinidadeAr;
+  String? feitico1;
+  String? feitico2;
+  String? feitico3;
+
+  Mago._({
+    required this.nome,
+    required this.idade,
+    required this.nivelMagico,
+    required this.inteligencia,
+    required this.mana,
+    required this.afinidadeFogo,
+    required this.afinidadeAgua,
+    required this.afinidadeTerra,
+    required this.afinidadeAr,
+    required this.feitico1,
+    required this.feitico2,
+    required this.feitico3,
+  });
+
+  factory Mago.fromJson(Map<String, dynamic> json) => Mago._(
+    nome: json['nome'],
+    idade: json['idade'],
+    nivelMagico: json['nivelMagico'],
+    inteligencia: json['inteligencia'],
+    mana: json['mana'],
+    afinidadeFogo: json['afinidadeFogo'],
+    afinidadeAgua: json['afinidadeAgua'],
+    afinidadeTerra: json['afinidadeTerra'],
+    afinidadeAr: json['afinidadeAr'],
+    feitico1: json['feitico1'],
+    feitico2: json['feitico2'],
+    feitico3: json['feitico3'],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "nome": nome,
+    "idade": idade,
+    "nivelMagico": nivelMagico,
+    "inteligencia": inteligencia,
+    "mana": mana,
+    "afinidadeFogo": afinidadeFogo,
+    "afinidadeAgua": afinidadeAgua,
+    "afinidadeTerra": afinidadeTerra,
+    "afinidadeAr": afinidadeAr,
+    "feitico1": feitico1,
+    "feitico2": feitico2,
+    "feitico3": feitico3,
+  };
+
+  String toString() =>
+      "MAGO ${nome} de ${idade} (lvl ${nivelMagico} e mana ${mana}) anos que tem os feitiços ${feitico1}, ${feitico2}, ${feitico3}";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,292 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:myapp/mago.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+const json = '''[
+    {
+        "nome": "Gandalf, o Cinzento",
+        "idade": 2000,
+        "nivelMagico": 15,
+        "inteligencia": 18.5,
+        "mana": 250,
+        "afinidadeFogo": 4,
+        "afinidadeAgua": 2,
+        "afinidadeTerra": 3,
+        "afinidadeAr": 7,
+        "feitico1": "Luz Ofuscante",
+        "feitico2": "Escudo Arcano",
+        "feitico3": "Chama de Anor"
+    },
+    {
+        "nome": "Merlin",
+        "idade": 832,
+        "nivelMagico": 20,
+        "inteligencia": 20,
+        "mana": 300,
+        "afinidadeFogo": 3,
+        "afinidadeAgua": 5,
+        "afinidadeTerra": 6,
+        "afinidadeAr": 4,
+        "feitico1": "Metamorfose",
+        "feitico2": "Visão do Futuro",
+        "feitico3": "Controle do Clima"
+    },
+    {
+        "nome": "Alatar, o Azul",
+        "idade": 1500,
+        "nivelMagico": 12,
+        "inteligencia": 17,
+        "mana": 220,
+        "afinidadeFogo": 1,
+        "afinidadeAgua": 8,
+        "afinidadeTerra": 2,
+        "afinidadeAr": 5,
+        "feitico1": "Torrente Congelante",
+        "feitico2": "Muralha de Gelo",
+        "feitico3": "Telepatia"
+    },
+    {
+        "nome": "Morgana",
+        "idade": 543,
+        "nivelMagico": 18,
+        "inteligencia": 19.5,
+        "mana": 280,
+        "afinidadeFogo": 6,
+        "afinidadeAgua": 3,
+        "afinidadeTerra": 2,
+        "afinidadeAr": 2,
+        "feitico1": "Controle Mental",
+        "feitico2": "Fogo das Sombras",
+        "feitico3": "Ilusão"
+    },
+    {
+        "nome": "Dumbledore",
+        "idade": 150,
+        "nivelMagico": 17,
+        "inteligencia": 19,
+        "mana": 270,
+        "afinidadeFogo": 5,
+        "afinidadeAgua": 4,
+        "afinidadeTerra": 3,
+        "afinidadeAr": 6,
+        "feitico1": "Patrono",
+        "feitico2": "Leglimência",
+        "feitico3": "Feitiço Fidelius"
+    },
+    {
+        "nome": "Elminster",
+        "idade": 1275,
+        "nivelMagico": 19,
+        "inteligencia": 19.8,
+        "mana": 290,
+        "afinidadeFogo": 7,
+        "afinidadeAgua": 3,
+        "afinidadeTerra": 4,
+        "afinidadeAr": 5,
+        "feitico1": "Palavra de Poder",
+        "feitico2": "Portal Dimensional",
+        "feitico3": "Chuva de Meteoros"
+    },
+    {
+        "nome": "Raistlin Majere",
+        "idade": 34,
+        "nivelMagico": 16,
+        "inteligencia": 18,
+        "mana": 200,
+        "afinidadeFogo": 8,
+        "afinidadeAgua": 2,
+        "afinidadeTerra": 1,
+        "afinidadeAr": 3,
+        "feitico1": "Sono",
+        "feitico2": "Bola de Fogo",
+        "feitico3": "Pele de Dragão"
+    },
+    {
+        "nome": "Medivh",
+        "idade": 403,
+        "nivelMagico": 19,
+        "inteligencia": 19.7,
+        "mana": 285,
+        "afinidadeFogo": 5,
+        "afinidadeAgua": 5,
+        "afinidadeTerra": 5,
+        "afinidadeAr": 5,
+        "feitico1": "Abrir Portal",
+        "feitico2": "Visão Remota",
+        "feitico3": "Transformação Corvo"
+    },
+    {
+        "nome": "Circe",
+        "idade": 950,
+        "nivelMagico": 16,
+        "inteligencia": 18.2,
+        "mana": 240,
+        "afinidadeFogo": 2,
+        "afinidadeAgua": 6,
+        "afinidadeTerra": 7,
+        "afinidadeAr": 1,
+        "feitico1": "Transformar em Animal",
+        "feitico2": "Poção de Amor",
+        "feitico3": "Crescimento Vegetal"
+    },
+    {
+        "nome": "Luna Estrela",
+        "idade": 27,
+        "nivelMagico": 8,
+        "inteligencia": 15.5,
+        "mana": 180,
+        "afinidadeFogo": 1,
+        "afinidadeAgua": 3,
+        "afinidadeTerra": 2,
+        "afinidadeAr": 9,
+        "feitico1": "Levitação",
+        "feitico2": "Sussurro do Vento",
+        "feitico3": "Salto Aéreo"
+    },
+    {
+        "nome": "Magnus Bane",
+        "idade": 412,
+        "nivelMagico": 14,
+        "inteligencia": 17.3,
+        "mana": 230,
+        "afinidadeFogo": 6,
+        "afinidadeAgua": 2,
+        "afinidadeTerra": 1,
+        "afinidadeAr": 4,
+        "feitico1": "Portal Dimensional",
+        "feitico2": "Glamour",
+        "feitico3": "Fogo Demoníaco"
+    },
+    {
+        "nome": "Tasha",
+        "idade": 340,
+        "nivelMagico": 15,
+        "inteligencia": 18.7,
+        "mana": 245,
+        "afinidadeFogo": 3,
+        "afinidadeAgua": 3,
+        "afinidadeTerra": 8,
+        "afinidadeAr": 2,
+        "feitico1": "Risada Hedionda",
+        "feitico2": "Recipiente Arcano",
+        "feitico3": "Convocação Demoníaca"
+    },
+    {
+        "nome": "Khadgar",
+        "idade": 68,
+        "nivelMagico": 13,
+        "inteligencia": 16.8,
+        "mana": 210,
+        "afinidadeFogo": 4,
+        "afinidadeAgua": 4,
+        "afinidadeTerra": 3,
+        "afinidadeAr": 4,
+        "feitico1": "Polimorfar",
+        "feitico2": "Mísseis Arcanos",
+        "feitico3": "Contrafeitiço"
+    },
+    {
+        "nome": "Jaina Proudmoore",
+        "idade": 39,
+        "nivelMagico": 12,
+        "inteligencia": 16.5,
+        "mana": 205,
+        "afinidadeFogo": 2,
+        "afinidadeAgua": 9,
+        "afinidadeTerra": 1,
+        "afinidadeAr": 3,
+        "feitico1": "Cone de Gelo",
+        "feitico2": "Blizzard",
+        "feitico3": "Barreira de Gelo"
+    },
+    {
+        "nome": "Taako",
+        "idade": 125,
+        "nivelMagico": 11,
+        "inteligencia": 16,
+        "mana": 190,
+        "afinidadeFogo": 3,
+        "afinidadeAgua": 1,
+        "afinidadeTerra": 2,
+        "afinidadeAr": 8,
+        "feitico1": "Mãos Mágicas",
+        "feitico2": "Disfarce Ilusório",
+        "feitico3": "Lufada Trovejante"
+    }
+]''';
+
+final List<dynamic> jsonList = jsonDecode(json);
+
+final List<Mago> magos = jsonList.map((value) => Mago.fromJson(value)).toList();
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Parseando JSON de Magos'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+
+        title: Text("Parseando JSON de Magos (Valentina Prado)"),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: Expanded(
+          child: ListView.builder(
+            itemCount: magos.length,
+            itemBuilder: (context, index) {
+              return SizedBox(
+                width: 100,
+                height: 150,
+                child: Column(
+                  children: [
+                    Text(
+                      magos[index].toString(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Afinidade Fogo: ${magos[index].afinidadeFogo}",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    Text(
+                      "Afinidade Água: ${magos[index].afinidadeAgua}",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    Text(
+                      "Afinidade Terra: ${magos[index].afinidadeTerra}",
+                      style: TextStyle(color: Colors.brown),
+                    ),
+                    Text(
+                      "Afinidade Ar: ${magos[index].afinidadeAr}",
+                      style: TextStyle(color: Colors.lightBlue),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
